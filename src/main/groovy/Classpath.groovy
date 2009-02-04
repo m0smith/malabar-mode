8ad721f4d8fe4c2557306fc9b9b2a69ab5263531
Espen Wiborg <espenhw@grumblesmurf.org>
2009-02-04 22:40:31 +0100

    Foo

---
+++ src/main/groovy/Classpath.groovy
@@ -0,0 +1,49 @@
+import org.codehaus.groovy.tools.RootLoader;
+
+class Classpath 
+{
+    def bootUrls = [];
+
+    def extUrls = [];
+    
+    def urls = [];
+    
+    Classpath() {
+        System.getProperty("sun.boot.class.path").split(File.pathSeparator).each() {
+            bootUrls << "file:" + it
+        }
+
+        System.getProperty("java.ext.dirs").split(File.pathSeparator).each() { dir ->
+            try {
+                (dir as File).eachFileMatch(~/.*.jar/) {
+                    extUrls << "file:" + it
+                }
+            } catch (FileNotFoundException e) {
+                // EAT IT
+            }
+        }
+    }
+
+    Classpath(entries) {
+        this()
+        entries.each{
+            if ((it as File).isDirectory())
+              it = it + "/"
+            this.urls << "file:" + it
+        }
+    }
+    
+    private classloader;
+    
+    def getClassLoader() {
+        if (classloader == null) {
+            URL[] realUrls = new URL[urls.size()];
+            urls.eachWithIndex() { it, i ->
+                realUrls[i] = new URL(it)
+            }
+            classloader = new RootLoader(realUrls,
+                                         ClassLoader.systemClassLoader.parent)
+        }
+        return classloader
+    }
+}
--- src/main/lisp/malabar-mode.el
+++ src/main/lisp/malabar-mode.el
@@ -17,7 +17,11 @@
 ;;
 
 (require 'semantic-load)
+(require 'semantic-ctxt)
+(require 'semantic-find)
 (require 'wisent-malabar-java-wy)
+(require 'cl)
+(require 'malabar-groovy)
 
 (define-mode-local-override semantic-get-local-variables
   malabar-mode ()
@@ -53,6 +57,41 @@
   (setq semantic-lex-depth 10)
   (setq semantic-lex-analyzer 'wisent-malabar-java-lexer)
   (wisent-malabar-java-wy--install-parser)
+  (semantic-idle-scheduler-mode 1)
   (remove-hook 'java-mode-hook 'wisent-java-default-setup))
+
+(defun malabar-type-token-candidates ()
+  (remove nil (mapcar (lambda (token)
+                        (when (eq (car token) 'IDENTIFIER)
+                          (buffer-substring-no-properties (cadr token) (cddr token))))
+                      (semantic-lex-buffer 1000))))
+
+(defun malabar-type-token-p (token)
+  (let ((case-fold-search nil))
+    (and (> (length token) 1)
+         (some (lambda (re)
+                 (string-match (concat "^" re "$") token))
+               java-font-lock-extra-types))))
+
+(defun malabar-class-defined-in-current-buffer-p (classname)
+  (let ((tags (semantic-find-tags-by-class 'type (current-buffer))))
+    (find classname tags
+          :key #'semantic-tag-name
+          :test #'equal)))
+
+(defun malabar-class-imported-p (classname)
+  (let ((tags (semantic-find-tags-by-class 'include (current-buffer))))
+    (find classname tags
+          :key (lambda (tag)
+                 (substring (semantic-tag-name tag)
+                            (1+ (position ?. (semantic-tag-name tag) :from-end t))))
+          :test #'equal)))
+
+(defun malabar-import-candidates ()
+  (let ((type-tokens (remove-if-not #'malabar-type-token-p (malabar-type-token-candidates))))
+    (remove-if (lambda (token)
+                 (or (malabar-class-defined-in-current-buffer-p token)
+                     (malabar-class-imported-p token)))
+               type-tokens)))
 
 (provide 'malabar-mode)
