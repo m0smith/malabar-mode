ed48e7b7bf4a27843d96bf0062e762ba7890d267
Espen Wiborg <espenhw@grumblesmurf.org>
2009-02-21 16:58:13 +0100

    Create from Maven POM

--- src/main/groovy/org/grumblesmurf/malabar/Project.groovy
+++ src/main/groovy/org/grumblesmurf/malabar/Project.groovy
@@ -1,22 +1,82 @@
 package org.grumblesmurf.malabar
 
-abstract class Project
+import org.apache.maven.embedder.MavenEmbedder;
+import org.apache.maven.execution.*;
+
+class Project
 {
     def compileClasspath;
     def runtimeClasspath;
     def testClasspath;
 
+    def resources;
+    def testResources;
+
     def name;
+    def description;
     def pomFile;
 
-    def srcDirectory;
+    def srcDirectories;
     def classesDirectory;
 
-    def testSrcDirectory;
+    def testSrcDirectories;
     def testClassesDirectory;
 
     def encoding = "UTF-8";
     def source = "1.3";
     def target = "1.1";
 
+    def mavenProject;
+    
+    static Project makeProject(pom) {
+        MavenEmbedder embedder = MvnServer.embedder
+        File pomFile = pom as File
+        MavenExecutionRequest req = MvnServer.newRequest()
+        req.baseDirectory = pomFile.parentFile
+        MavenExecutionResult result = embedder.readProjectWithDependencies(req)
+        if (result.hasExceptions()) {
+            // handle exceptions
+            println '(error "%s" "' + result.exceptions + '")'
+            return null;
+        } else if (result.artifactResolutionResult.missingArtifacts) {
+            println '(error "Missing artifacts: %s" "' + result.artifactResolutionResult.missingArtifacts + '")'
+            return null;
+        } else {
+            Project me = new Project(pom, result);
+            Projects.put(pom, me)
+            return me;
+        }
+    }
+    
+    private Project(pom, result) {
+        pomFile = pom
+        mavenProject = result.project
+        name = mavenProject.name
+        description = mavenProject.description
+
+        srcDirectories = mavenProject.compileSourceRoots
+        classesDirectory = mavenProject.build.outputDirectory
+        resources = mavenProject.resources.collect{
+            // TODO: better resource handling
+            it.directory
+        }
+
+        testSrcDirectories = mavenProject.testCompileSourceRoots
+        testClassesDirectory = mavenProject.build.testOutputDirectory
+        testResources = mavenProject.testResources.collect{
+            // TODO: better resource handling
+            it.directory
+        }
+
+        compileClasspath = new Classpath(mavenProject.compileClasspathElements);
+        testClasspath = new Classpath(mavenProject.testClasspathElements);
+        runtimeClasspath = new Classpath(mavenProject.runtimeClasspathElements);
+
+        def compilerConfig = mavenProject.getPlugin("org.apache.maven.plugins:maven-compiler-plugin").configuration
+        if (compilerConfig) {
+            encoding = (compilerConfig.getChild("encoding") ?: encoding).value
+            source = (compilerConfig.getChild("source") ?: source).value
+            target = (compilerConfig.getChild("target") ?: target).value
+        }
+    }
 }
