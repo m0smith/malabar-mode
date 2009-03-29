d0b0f1e54fa878cb7f6467ee70289c912239ec3d
Espen Wiborg <espenhw@grumblesmurf.org>
2009-03-29 18:44:48 +0200

    Require srecode-map to fix load error.

--- src/main/lisp/malabar-variables.el
+++ src/main/lisp/malabar-variables.el
@@ -17,7 +17,7 @@
 ;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 ;; 02110-1301 USA.
 ;;
-(require 'srecode)
+(require 'srecode-map)
 
 (defgroup malabar-mode nil
   "A better Java mode")
