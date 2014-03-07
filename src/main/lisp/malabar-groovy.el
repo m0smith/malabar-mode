a525edab0938d3a7ba15a139e4331f93c2a5b275
Matthew O. Smith <matt@m0smith.com>
2014-03-07 15:18:22 -0700

    Added a way to run a java main method in the current buffer.

--- src/main/lisp/malabar-groovy.el
+++ src/main/lisp/malabar-groovy.el
@@ -361,6 +361,15 @@ for it to come up."
 	 )
     rtnval))
 
+(defun malabar-groovy-run-main (&optional class args)
+  "Run the main method of the given class
+TODO: args is basically ignored
+TODO: capture stdout and stderr"
+  (interactive)
+  (let ((class (or class (malabar-get-fully-qualified-class-name (current-buffer))))
+	(args (or args "new String[0]")))
+    (malabar-eval-on-project (format "runMain('%s', %s)" class args))))
+
 
 (defcustom malabar-groovy-compilation-font-lock-keywords
   '((malabar-groovy-highlight-compilation-message
